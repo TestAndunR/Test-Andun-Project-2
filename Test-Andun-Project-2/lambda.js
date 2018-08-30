e00a8c285656143c8c16ba71c9b2c222553b9b7f
TestAndunR <andunranmal@gmail.com>
2018-08-30 16:02:44 +0530

    3rd commit

--- Test-Andun-Project-2/lambda.js
+++ Test-Andun-Project-2/lambda.js
@@ -1,6 +1,7 @@
 let AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
-    console.log("2nd commit")
+    console.log("2nd commit");
+    console.log("3rd commit");
     callback(null,'Successfully executed');
 }
